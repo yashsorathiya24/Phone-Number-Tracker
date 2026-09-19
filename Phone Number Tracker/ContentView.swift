44a420610e67ef554b20e869c0b0ca0e296129cf
Yash Sorathiya <yp73280@gmail.com>
2026-09-19 09:38:37 +0530

    Initial commit

---
+++ Phone Number Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Phone Number Tracker
+//
+//  Created by Yash Sorathiya  on 19/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Phone Number Tracker/Phone_Number_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phone_Number_TrackerApp.swift
+//  Phone Number Tracker
+//
+//  Created by Yash Sorathiya  on 19/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Phone_Number_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
